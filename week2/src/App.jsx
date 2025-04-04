0ac4fb2f489b210c223fb92207a11e5c9786ff0a
kdhdd <kdso10@naver.com>
2025-04-04 16:40:19 +0900

    :sparkles: 피드백 수정

--- week2/src/App.jsx
+++ week2/src/App.jsx
@@ -1,67 +1,80 @@
-import "./App.css"; 
-import 'bootstrap/dist/css/bootstrap.min.css';
- 
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 export default function App() {
   return (
     <div>
       <header className="header">
         <div className="intro-text">
-            <div className="intro-lead-in">
-                My Profile
-            </div>
-            <div className="intro-heading">
-                나만의 웹사이트를 만들어봐요!
-            </div>
+          <div className="intro-lead-in">My Profile</div>
+          <div className="intro-heading">나만의 웹사이트를 만들어봐요!</div>
         </div>
       </header>
 
       <nav className="navbar fixed-top">
-          <ul>
-              <li><a href="#about">자기소개</a></li>
-              <li><a href="#features">관심사</a></li>
-              <li><a href="#contact">연락하기</a></li>
-              <li><a href="https://skulikelion.com" target="_blank">멋사 사이트 바로가기</a></li>
-          </ul>
+        <ul>
+          <li>
+            <a href="#about">자기소개</a>
+          </li>
+          <li>
+            <a href="#features">관심사</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="https://github.com/kdhdd" target="_blank">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+        </ul>
       </nav>
 
       <main className="main">
-          <section id="about" className="section">
-              <h2>안녕하세요!</h2>
-              <p>만나서 반갑습니다. 저는 컴퓨터공학과에 재학중인 22학번 홍길동입니다.</p>
-              <p>함께 즐거운 동아리 생활 보내봐요 ^^</p>
-              <img className="img" src="https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/eViY/image/is3UFbmo7Gwuiln43EznmVZFwXo.jpg" alt="about_image" />
-          </section>
+        <section id="about" className="section">
+          <img className="img" src="미모티콘.jpg" alt="about_image" />
+          <h2>
+            프론트엔드 개발자 <b>김도현</b>입니다.
+          </h2>
+        </section>
 
-          <section id="features" className="section">
-              <h2>관심사</h2>
-              
-              <h3>취미</h3>
-              <ul>
-                  <li>농구 보기</li>
-                  <li>음악 감상</li>
-                  <li>기타 치기</li>
-                  <li>코딩 ^^</li>
-              </ul>
-              <img className="img" src="https://i.ytimg.com/vi/dIt_N7yNTgk/mqdefault.jpg" alt="guitar_image" />
+        <section id="features" className="section">
+          <h3>취미</h3>
+          <ul>
+            <li>게임 하기</li>
+            <li>음악 감상</li>
+            <li>운동 하기</li>
+          </ul>
+          <img
+            className="img"
+            src="https://static.hubzum.zumst.com/hubzum/2020/11/03/09/d534a7eb69ed4201b756a579192b6f8c.jpg"
+            alt="guitar_image"
+          />
 
-              <h3>좋아하는 음식 순위</h3>
-              <ol>
-                  <li>타코</li>
-                  <li>떡볶이</li>
-                  <li>햄버거</li>
-              </ol>
-              <img className="img" src="https://i.pinimg.com/474x/7a/9e/ec/7a9eec73158b3e0a84691551180fef8e.jpg" alt="hamburger_image" />
-          </section>
+          <h3>좋아하는 음식 순위</h3>
+          <ol>
+            <li>피자</li>
+            <li>치킨</li>
+            <li>햄버거</li>
+          </ol>
+          <img
+            className="img"
+            src="http://www.youngmanpizza.co.kr/upload/M_ori/6678d099abe9c.jpg"
+            alt="hamburger_image"
+          />
+        </section>
 
-          <section id="contact" className="section">
-              <h2>연락하기</h2>
-              <p>이메일: skulikelion@example.com</p>
-              <p>전화번호: 010-1234-5678</p>
-          </section>
+        <section id="contact" className="section">
+          <h2>연락하기</h2>
+          <p>이메일: kdso10@skuniv.ac.kr</p>
+          <p>전화번호: 010-5393-3821</p>
+        </section>
       </main>
 
       <footer className="footer">
-          <p>© 2025. SKU LIKELION. All rights reserved.</p>
+        <p>© 2025. SKU LIKELION. All rights reserved.</p>
       </footer>
     </div>
   );
